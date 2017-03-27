605a33259b36132e01f6cd978c47c7f01e687ed1
nathanieltan <nathaniel98.tan@gmail.com>
2017-03-27 19:59:43 -0400

    Submitting toolbox

--- blues_solo.py
+++ blues_solo.py
@@ -12,6 +12,9 @@ SAMPLES_DIR = os.path.join(os.path.dirname(__file__), "samples")
 SAMPLE_FILE = os.path.join(SAMPLES_DIR, "bass_D2.wav")
 SAMPLE_NOTE = D2  # the sample file plays at this pitch
 
+BACKING_TRACK = os.path.join(SAMPLES_DIR, "backing.wav")
+sample(BACKING_TRACK, amp=2)
+sleep(2.25)  # delay the solo to match up with backing track
 
 def play_note(note, beats=1, bpm=60, amp=1):
     """Plays note for `beats` beats. Returns when done."""
@@ -39,4 +42,19 @@ atexit.register(stop)  # stop all tracks when the program exits normally or is i
 blues_scale = [40, 43, 45, 46, 47, 50, 52, 55, 57, 58, 59, 62, 64, 67, 69, 70, 71, 74, 76]
 beats_per_minute = 45				# Let's make a slow blues solo
 
-play_note(blues_scale[0], beats=1, bpm=beats_per_minute)
+curr_note = 0
+play_note(blues_scale[curr_note], 1, beats_per_minute)
+licks = [[(1, 0.5*1.3), (1, 0.5*0.7), (1, 0.5*1.3), (1, 0.5*0.7)],[(-1, 0.5), (-1, 0.5), (-1, 0.5), (-1, 0.5)]]
+licks.append([(1, 0.5 * 1.1), (1, 0.5 * 0.9), (1, 0.5 * 1.1), (1, 0.5 * 0.9)])
+licks.append([(1, 0.5*1.5),(-1,0.5*0.5),(1,0.5*1.1),(-1,0.5*0.9)])
+
+for _ in range(4):
+    lick = random.choice(licks)
+    for note in lick:
+        if curr_note + note[0] < 0:
+            curr_note = 0
+        elif curr_note +note[0] > len(blues_scale) - 1:
+            curr_note = len(blues_scale) - 1
+        else:
+            curr_note += note[0]
+        play_note(blues_scale[curr_note], note[1], beats_per_minute)
